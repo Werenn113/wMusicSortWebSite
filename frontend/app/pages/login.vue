6c303958b9d2f1db7d73b5b5e9f85eabec28993f
Werenn <baptm006@gmail.com>
2025-12-09 14:08:04 +0000

    push pour pouvoir passer sur un autre ordi

--- frontend/app/pages/login.vue
+++ frontend/app/pages/login.vue
@@ -1,34 +1,44 @@
 <script setup lang="ts">
-import * as v from 'valibot'
 import type { FormSubmitEvent } from '@nuxt/ui'
+import { userStore } from '~/stores/users'
+import zod from 'zod'
 
-const schema = v.object({
-  email: v.pipe(v.string(), v.email('Invalid email')),
-  password: v.pipe(v.string(), v.minLength(8, 'Must be at least 8 characters'))
+const schema = zod.object({
+  email: zod.email('Invalid email'),
+  password: zod.string('Password is required').min(8, 'Must be at least 8 characters')
 })
 
-type Schema = v.InferOutput<typeof schema>
+type Schema = zod.output<typeof schema>
 
-const state = reactive({
-  email: '',
-  password: ''
+const loginInformations = reactive<Partial<Schema>>({
+  email: undefined,
+  password: undefined
 })
 
+const users = userStore()
 const toast = useToast()
+const router = useRouter()
+
 async function onSubmit(event: FormSubmitEvent<Schema>) {
-  toast.add({ title: 'Success', description: 'The form has been submitted.', color: 'success' })
-  console.log(event.data)
+  try {
+    await users.authenticate(event.data)
+    toast.add({ title: 'Connexion réussie', description: 'Vous êtes maintenant connecté.', color: 'success' })
+    // router.push('/')
+  } catch (error: any) {
+    toast.add({ title: 'Erreur de connexion', description: error.message || 'Une erreur est survenue lors de la connexion.', color: 'error' })
+    console.error('Erreur d\'authentification:', error)
+  }
 }
 </script>
 
 <template>
-  <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
+  <UForm :schema="schema" :state="loginInformations" class="space-y-4" @submit="onSubmit">
     <UFormField label="Email" name="email">
-      <UInput v-model="state.email" />
+      <UInput v-model="loginInformations.email" />
     </UFormField>
 
     <UFormField label="Password" name="password">
-      <UInput v-model="state.password" type="password" />
+      <UInput v-model="loginInformations.password" type="password" />
     </UFormField>
 
     <UButton type="submit">
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,17 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-
   modules: [
-    '@nuxt/content',
     '@nuxt/eslint',
-    '@nuxt/hints',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    '@nuxt/ui'
+    '@nuxt/ui',
+    '@pinia/nuxt'
   ],
 
-  css: ['~/assets/css/main.css']
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  compatibilityDate: '2025-01-15',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
 })
